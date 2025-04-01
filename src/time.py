ee7673888ae75c0b5b556164ae8b7c7045470af3
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-01 14:59:02 +0900

    [update] 不要なファイルを削除

--- src/time.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-t = pd.read_csv('time.csv')
-t = t[t["cpu"] > 1.5e6]
-print(t.head())
-plt.hist(t['real'], bins=20)
-plt.hist(t['cpu'], bins=20, alpha=0.5)
-plt.show()
