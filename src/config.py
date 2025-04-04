5c8da694b24008f86416421c626d4a2c57e08d6b
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-04 21:29:51 +0900

    [add] src/config.py

---
+++ src/config.py
@@ -0,0 +1,33 @@
+import typing as t
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+
+
+class WindPowerLow:
+    reference_height: float
+    wind_speed: float
+    exponent: float
+    wind_direction: float
+
+
+@dataclass
+class Config:
+    mass: pd.DataFrame
+    wind: WindPowerLow
+    thrust: pd.DataFrame
+    CA: float
+    CN_alpha: float
+    body_area: float
+    wind_center: np.ndarray
+    dt: float
+    launcher_length: float
+    inertia_tensor_xx: float
+    inertia_tensor_yy: float
+    inertia_tensor_zz: float
+    inertia_tensor_zy: float
+    inertia_tensor_xz: float
+    inertia_tensor_xy: float
+    first_elevation: float
+    first_azimuth: float
+    first_roll: float
