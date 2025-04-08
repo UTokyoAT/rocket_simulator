ea0f7762a90a992de1653960743ed350e3cbdacf
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-08 16:50:38 +0900

    [update] simple_simulationがSimulationContextを利用するように変更

--- src/core/simple_simulation.py
+++ src/core/simple_simulation.py
@@ -5,10 +5,11 @@ from . import quaternion_util
 from . import ode_solver
 from . import equation_of_motion
 from . import simulation_result
+from .simulation_context import SimulationContext
 
 
 def air_force_body_frame(
-    rocket_state: RocketState, config: Config
+    rocket_state: RocketState, context: SimulationContext
 ) -> tuple[np.ndarray, np.ndarray]:
     """機体座標系での風圧による力を計算する
 
@@ -21,17 +22,17 @@ def air_force_body_frame(
     """
     airspeed = quaternion_util.inertial_to_body(
         rocket_state.posture,
-        rocket_state.velocity - config.wind(-rocket_state.position[2]),
+        rocket_state.velocity - context.wind(-rocket_state.position[2]),
     )
     angle_of_attack = af.angle_of_attack(airspeed)
     air_density = 1.204
-    axial_force = af.axial_force(airspeed, air_density, config.body_area, config.CA)
-    CN = af.normal_force_coefficient(angle_of_attack, config.CN_alpha)
-    normal_force = af.normal_force(airspeed, air_density, config.body_area, CN)
+    axial_force = af.axial_force(airspeed, air_density, context.body_area, context.CA)
+    CN = af.normal_force_coefficient(angle_of_attack, context.CN_alpha)
+    normal_force = af.normal_force(airspeed, air_density, context.body_area, CN)
     air_force = axial_force + normal_force
     moment = af.air_force_moment(
         air_force,
-        config.wind_center,
+        context.wind_center,
     )
     return air_force, moment
 
@@ -52,7 +53,7 @@ def to_simulation_result_row(
 
 
 def simulate_launcher(
-    first_state: RocketState, config: Config, first_time: float
+    first_state: RocketState, context: SimulationContext, first_time: float
 ) -> simulation_result.SimulationResult:
     """ランチャー上でのシミュレーションを行う
 
@@ -66,32 +67,35 @@ def simulate_launcher(
     """
 
     def derivative(t, state):
-        air_force, _ = air_force_body_frame(state, config)
-        thrust = np.array([config.thrust(t), 0, 0])
+        air_force, _ = air_force_body_frame(state, context)
+        thrust = np.array([context.thrust(t), 0, 0])
         virtual_force_body = quaternion_util.sum_vector_body_frame(
             [air_force, thrust],
-            [Gravitational_acceleration * config.mass(t)],
+            [Gravitational_acceleration * context.mass(t)],
             state.posture,
         )
         actual_force_inertial = quaternion_util.body_to_inertial(
             state.posture, np.array([max(0, virtual_force_body[0]), 0, 0])
         )
         return RocketState.derivative(
-            state, actual_force_inertial / config.mass(t), np.zeros(3)
+            state, actual_force_inertial / context.mass(t), np.zeros(3)
         )
 
     def end_condition(t, state):
-        return np.linalg.norm(state.position, ord=2) > config.launcher_length
+        return np.linalg.norm(state.position, ord=2) > context.launcher_length
 
     result = ode_solver.runge_kutta4(
-        derivative, first_state, first_time, config.dt, end_condition
+        derivative, first_state, first_time, context.dt, end_condition
     )
     result = map(lambda row: to_simulation_result_row(*row), result)
     return simulation_result.SimulationResult(list(result))
 
 
 def simulate_flight(
-    first_state: RocketState, config: Config, first_time: float, parachute_on: float
+    first_state: RocketState,
+    context: SimulationContext,
+    first_time: float,
+    parachute_on: float,
 ) -> simulation_result.SimulationResult:
     """飛行中のシミュレーションを行う
 
@@ -105,14 +109,14 @@ def simulate_flight(
     """
 
     def derivative(t, state):
-        air_force, moment = air_force_body_frame(state, config)
-        thrust = np.array([config.thrust(t), 0, 0])
+        air_force, moment = air_force_body_frame(state, context)
+        thrust = np.array([context.thrust(t), 0, 0])
         force = quaternion_util.sum_vector_inertial_frame(
             [air_force, thrust], [np.zeros(3)], state.posture
         )
-        acceleration = force / config.mass(t) + Gravitational_acceleration
+        acceleration = force / context.mass(t) + Gravitational_acceleration
         angular_acceleration = equation_of_motion.angular_acceleration(
-            moment, config.inertia_tensor, state.rotation
+            moment, context.inertia_tensor, state.rotation
         )
         return RocketState.derivative(state, acceleration, angular_acceleration)
 
@@ -123,13 +127,13 @@ def simulate_flight(
             raise NotImplementedError("パラシュートを開いた時は未実装")
 
     result = ode_solver.runge_kutta4(
-        derivative, first_state, first_time, config.dt, end_condition
+        derivative, first_state, first_time, context.dt, end_condition
     )
     result = map(lambda row: to_simulation_result_row(*row), result)
     return simulation_result.SimulationResult(list(result))
 
 
-def simulate(config: Config, parachute_on: float) -> simulation_result.SimulationResult:
+def simulate(context: SimulationContext, parachute_on: float) -> simulation_result.SimulationResult:
     """全体のシミュレーションを行う
 
     Args:
@@ -140,10 +144,10 @@ def simulate(config: Config, parachute_on: float) -> simulation_result.Simulatio
         simulation_result.SimulationResult: 時刻とロケットの状態の組のリストをモードごとに格納したリスト
     """
     first_posture = quaternion_util.from_euler_angle(
-        config.first_elevation, config.first_azimuth, config.first_roll
+        context.first_elevation, context.first_azimuth, context.first_roll
     )
     first_state = RocketState(np.zeros(3), np.zeros(3), first_posture, np.zeros(3))
-    result_launcher = simulate_launcher(first_state, config, 0)
+    result_launcher = simulate_launcher(first_state, context, 0)
     print("ランチャー上でのシミュレーション終了")
     print(result_launcher.last())
     last = result_launcher.last()
@@ -153,7 +157,7 @@ def simulate(config: Config, parachute_on: float) -> simulation_result.Simulatio
         last.posture,
         last.rotation,
     )
-    result_flight = simulate_flight(first_state, config, last.time, parachute_on)
+    result_flight = simulate_flight(first_state, context, last.time, parachute_on)
     if parachute_on:
         raise NotImplementedError("パラシュートを開いた時は未実装")
     else:
