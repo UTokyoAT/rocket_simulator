ae305f9a2da7df14011201d6be035d38dec5fc42
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-13 09:24:51 +0900

    [add] src/report_config_read.py

---
+++ src/report_config_read.py
@@ -0,0 +1,14 @@
+import os
+import json
+from .make_report.make_result_for_report import ReportConfig
+
+
+def read(folder_path: str) -> ReportConfig:
+    with open(os.path.join(folder_path, "report_config.json"), "r") as file:
+        js = json.load(file)
+    return ReportConfig(
+        wind_speed_nominal=js["wind_speed_nominal"],
+        wind_direction_nominal=js["wind_direction_nominal"],
+        wind_speed_list=js["wind_speed_list"],
+        wind_direction_list=js["wind_direction_list"],
+    )
