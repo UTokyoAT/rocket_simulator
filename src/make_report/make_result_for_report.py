b28fcbea3904fb50373d3bc823f216e3db2428e1
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-13 09:10:29 +0900

    [fix] ReportConfigの不足していたパラメータを追加

--- src/make_report/make_result_for_report.py
+++ src/make_report/make_result_for_report.py
@@ -10,8 +10,10 @@ from ..core.config import Config
 
 @dataclass
 class ReportConfig:
-    wind_speed_nominal: float
+    wind_speed_ideal: float
     wind_direction_ideal: float
+    wind_speed_nominal: float
+    wind_direction_nominal: float
     wind_speed_list: list[float]
     wind_direction_list: list[float]
 
