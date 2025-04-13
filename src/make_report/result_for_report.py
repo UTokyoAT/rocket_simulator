9a6cc0c9784bb5a15f57a56321a2241289f022a0
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-13 09:04:15 +0900

    [add] ResultForReportにappendを追加

--- src/make_report/result_for_report.py
+++ src/make_report/result_for_report.py
@@ -15,6 +15,11 @@ class ResultByWindSpeed:
     wind_speed: float
     result: list[ResultByWindDirection]
 
+    def append(self, wind_direction: float, result: SimulationResult):
+        self.result.append(
+            ResultByWindDirection(wind_direction=wind_direction, result=result)
+        )
+
 
 @dataclass
 class ResultForReport:
@@ -23,3 +28,15 @@ class ResultForReport:
     result_ideal: SimulationResult
     result_nominal: SimulationResult
     result_by_wind_speed: list[ResultByWindSpeed]
+
+    def append(self, wind_speed: float, wind_direction: float, result: SimulationResult):
+        for result_by_wind_speed in self.result_by_wind_speed:
+            if result_by_wind_speed.wind_speed == wind_speed:
+                result_by_wind_speed.append(wind_direction, result)
+                return
+        self.result_by_wind_speed.append(
+            ResultByWindSpeed(
+                wind_speed=wind_speed,
+                result=[ResultByWindDirection(wind_direction=wind_direction, result=result)],
+            )
+        )
