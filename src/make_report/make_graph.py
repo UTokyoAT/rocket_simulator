dfd81bf69af0304d0ab0f5fd1cfee1209bbe3c79
TatsuyaKamikawa <kamikawa-tatsuya915@g.ecc.u-tokyo.ac.jp>
2025-04-17 20:33:19 +0900

    [fix] 関数dynamic_pressureをdynamic_pressure_figureに改名

--- src/make_report/make_graph.py
+++ src/make_report/make_graph.py
@@ -10,12 +10,94 @@ class Graphs:
     ideal_dynamic_pressure: Figure
     ideal_air_velocity_figure: Figure
 
+#def velocity_norm(row):
+    #return (row.vel_NED_x**2 + row.vel_NED_y**2 + row.vel_NED_z**2) ** 0.5
+
+
+#def acc_norm(row):
+    #return (row.acc_BODY_x**2 + row.acc_BODY_y**2 + row.acc_BODY_z**2) ** 0.5
+
+
+#def air_velocity_norm(row):
+    #return (
+       #row.vel_AIR_BODY_x**2 + row.vel_AIR_BODY_y**2 + row.vel_AIR_BODY_z**2
+    #) ** 0.5
+
+
+#def mode_change_row(data: pd.DataFrame, mode):
+    #return data[data["mode"] == mode].iloc[0]
+
+
+#def thrust_end_time():
+    #thrust = pd.read_csv("simulator/files/thrust.csv")
+    #thrust_not_first = thrust.iloc[10:]
+    #return thrust_not_first[thrust_not_first["thrust"] == 0].iloc[0]["time"]
+
+
+#def document_config():
+    #with open("simulator/files/審査書用コンフィグ.json") as f:
+        #return json.load(f)
+
+
+#def flst():
+    #with open("simulator/files/flst.json") as f:
+        #return json.load(f)
+
+
+#def to_image(ax):
+    #buf = BytesIO()
+    #ax.figure.savefig(buf, format="png")
+    #buf.seek(0)
+    #return Image.open(buf)
+
+
+#def launch_clear(data: pd.DataFrame):
+    """ランチクリア時の情報"""
+
+    #launch_clear = mode_change_row(flight_data, 1)
+    #v = velocity_norm(launch_clear)
+    #theta = np.deg2rad(document_config()["最小射角"])
+    #alpha = np.deg2rad(21)
+    #beta = np.deg2rad(20)
+    #w_alpha = v * np.tan(alpha) / (np.sin(theta) + np.cos(theta) * np.tan(alpha))
+    #w_beta = v * np.tan(beta)
+    #if v < 15:
+        #print("ランチクリア速度が遅すぎます．打ち上げできません")
+    #return {
+        #"時刻/s": round(launch_clear.time, 2),
+        #"速度/(m/s)": round(v, 2),
+        #"順風迎角21degの時の風速/(m/s)": round(w_alpha, 2),
+        #"側風迎角20degの時の風速/(m/s)": round(w_beta, 2),
+        #"風速制限/(m/s)": round(min(w_alpha, w_beta), 2),
+    #}
 def burning_coasting_division(data: pd.DataFrame):
     burning = data[data['burning'] == True]
     coasting = data[data['burning'] == False]
     return burning, coasting
 
-def dynamic_pressure(data: pd.DataFrame) -> Figure:
+def dynamic_pressure(flight_data, all=False):
+    burning, coasting = burning_coasting_division(flight_data)
+    # plt.plot(burning["time"],burning["dynamic_pressure"],label="burning")
+    # plt.plot(coasting["time"],coasting["dynamic_pressure"],label="coasting")
+    # plt.legend()
+    # # plt.title("dynamic pressure")
+    # plt.xlabel("time/s")
+    # plt.ylabel("dynamic pressure/Pa")
+    # plt.grid(which="both")
+
+    # plt.savefig("dynamic_pressure.png")
+    # plt.clf()
+    if all:
+        pressure_max = flight_data.loc[flight_data["dynamic_pressure"].idxmax()]
+    else:
+        pressure_max = burning.loc[burning["dynamic_pressure"].idxmax()]
+    return {
+        "時刻/s": round(pressure_max.time, 2),
+        "高度/m": round(pressure_max.altitude, 2),
+        "動圧/kPa": round(pressure_max.dynamic_pressure / 1000, 2),
+        "対気速度/(m/s)": round(air_velocity_norm(pressure_max), 2),
+    }
+def dynamic_pressure_figure(data: pd.DataFrame) -> Figure:
     fig, ax = plt.subplots()
     burning, coasting = burning_coasting_division(data)
     fig, ax = plt.subplots()
@@ -45,6 +127,6 @@ def air_velocity_figure(data: pd.DataFrame) -> Figure:
 
 def make_graph(result: ResultForReport) -> Graphs:
     return Graphs(
-        ideal_dynamic_pressure = dynamic_pressure(result.result_ideal),
+        ideal_dynamic_pressure = dynamic_pressure_figure(result.result_ideal),
         ideal_air_velocity_figure = air_velocity_figure(result.result_ideal),
     )
