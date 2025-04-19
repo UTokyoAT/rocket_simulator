65989fa59ea054a87439b789e8fb76c9903b2f7e
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-19 13:43:16 +0900

    [fix] マージ時にto_dfを余分に呼び出していたのを修正

--- scripts/make_report.py
+++ scripts/make_report.py
@@ -9,29 +9,29 @@ from src import graph_writer
 def write_row_data(result: ResultForReport):
     output_dir = os.path.join(os.path.abspath("output"), "report", "raw")
     os.makedirs(output_dir, exist_ok=True)
-    result.result_ideal_parachute_off.to_df().to_csv(
+    result.result_ideal_parachute_off.to_csv(
         os.path.join(output_dir, "ideal_parachute_off.csv"),
     )
-    result.result_ideal_parachute_on.to_df().to_csv(
+    result.result_ideal_parachute_on.to_csv(
         os.path.join(output_dir, "ideal_parachute_on.csv"),
     )
-    result.result_nominal_parachute_off.to_df().to_csv(
+    result.result_nominal_parachute_off.to_csv(
         os.path.join(output_dir, "nominal_parachute_off.csv"),
     )
-    result.result_nominal_parachute_on.to_df().to_csv(
+    result.result_nominal_parachute_on.to_csv(
         os.path.join(output_dir, "nominal_parachute_on.csv"),
     )
     for result_by_launcher_elevation in result.result_by_launcher_elevation:
         for result_by_wind in result_by_launcher_elevation.result:
             for result_by_direction in result_by_wind.result:
                 name = f"launcher_elevation_{result_by_launcher_elevation.launcher_elevation}_wind_speed_{result_by_wind.wind_speed}_wind_direction_{result_by_direction.wind_direction}"
-                result_by_direction.result_parachute_off.to_df().to_csv(
+                result_by_direction.result_parachute_off.to_csv(
                     os.path.join(
                         output_dir,
                         name + "_parachute_off.csv",
                     )
                 )
-                result_by_direction.result_parachute_on.to_df().to_csv(
+                result_by_direction.result_parachute_on.to_csv(
                     os.path.join(
                         output_dir,
                         name + "_parachute_on.csv",
