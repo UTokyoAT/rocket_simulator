da7d1e7b9742e7fa8046e2357ec532fbb9498ecf
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-26 20:57:11 +0900

    [fix] test_rocket_state.pyのリンター警告を修正

--- tests/test_rocket_state.py
+++ tests/test_rocket_state.py
@@ -7,7 +7,7 @@ from src.core.rocket_state import RocketState
 
 
 class TestRocketState(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.rs1 = RocketState(
             np.array([1, 2, 3]),
             np.array([2, 3, 4]),
@@ -21,15 +21,18 @@ class TestRocketState(unittest.TestCase):
             np.array([8, 9, 10]),
         )
 
-    def assertRocketStateEqual(self, rs, position, velocity, posture, rotation):
+    def assert_rocket_state_equal(
+        self, rs: RocketState, position: np.ndarray, velocity: np.ndarray,
+        posture: quart.quaternion, rotation: np.ndarray,
+    ) -> None:
         np.testing.assert_array_equal(rs.position, position)
         np.testing.assert_array_equal(rs.velocity, velocity)
         np.testing.assert_array_equal(rs.posture, posture)
         np.testing.assert_array_equal(rs.rotation, rotation)
 
-    def test_add(self):
+    def test_add(self) -> None:
         result = self.rs1 + self.rs2
-        self.assertRocketStateEqual(
+        self.assert_rocket_state_equal(
             result,
             [6, 8, 10],
             [8, 10, 12],
@@ -37,9 +40,9 @@ class TestRocketState(unittest.TestCase):
             [12, 14, 16],
         )
 
-    def test_mul(self):
+    def test_mul(self) -> None:
         result = self.rs1 * 2
-        self.assertRocketStateEqual(
+        self.assert_rocket_state_equal(
             result, [2, 4, 6], [4, 6, 8], quart.quaternion(2, 4, 6, 8), [8, 10, 12],
         )
 
