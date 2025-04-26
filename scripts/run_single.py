d269ceb61c22b171457b2e804d1d82afa22b096c
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-04-26 19:54:57 +0900

    [fix] run_single.pyのリンター警告を修正

--- scripts/run_single.py
+++ scripts/run_single.py
@@ -4,6 +4,6 @@ from src import config_read
 from src.core import simple_simulation
 
 config = config_read.read(Path("config"))
-result = simple_simulation.simulate(config, False)
+result = simple_simulation.simulate(config, parachute_on=False)
 Path("output").mkdir(exist_ok=True)
 result.to_df().to_csv("output/result.csv")
