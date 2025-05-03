07898f6b3620b6ae9a0386f47f778c4b05b96d70
TatsuyaKamikawa <kamikawa-tatsuya915@g.ecc.u-tokyo.ac.jp>
2025-05-03 10:08:22 +0900

    [add]altitude_downrange_figureを追加

--- src/make_report/make_graph.py
+++ src/make_report/make_graph.py
@@ -205,7 +205,4 @@ def make_graph(result: ResultForReport) -> Graphs:
     return Graphs(
         ideal_dynamic_pressure = dynamic_pressure_figure(result.result_ideal_parachute_off),
         ideal_air_velocity_figure = air_velocity_figure(result.result_ideal_parachute_off),
-        ideal_altitude_downrange_figure = altitude_downrange_figure(result.result_ideal_parachute_off),
-        ideal_time_altitude_figure = time_altitude_figure(result.result_ideal_parachute_off),
-        ideal_altitude_downrange_figure = altitude_downrange_figure(result.result_ideal_parachute_off),
     )
