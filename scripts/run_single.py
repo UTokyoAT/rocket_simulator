49df4e890a57286fadcd4faa596e197a8abdb63e
TohyamaHaruki <tohyama-haruki1212@g.ecc.u-tokyo.ac.jp>
2025-06-25 22:01:25 +0900

    [update] 不要なスクリプトを削除

--- scripts/run_single.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from src import config_read
-from src.core import simple_simulation
-
-config = config_read.read(Path("config"))
-result = simple_simulation.simulate(config, parachute_on=False)
-Path("output").mkdir(exist_ok=True)
-result.to_df().to_csv("output/result.csv")
